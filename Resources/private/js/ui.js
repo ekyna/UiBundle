6562065485d22a130bbfea6d238f6e454dec82c6
Etienne Dauvergne <contact@ekyna.com>
2022-03-02 15:52:12 +0100

    [Ui] Improved JS. Fixed select2 focus once opened.

--- Resources/private/js/ui.js
+++ Resources/private/js/ui.js
@@ -1,12 +1,12 @@
 define(['require', 'jquery', 'aos', 'bootstrap', 'ekyna-clipboard-copy'], function(require, $, AOS) {
 
-    var EkynaUi = function() {};
+    let EkynaUi = function() {};
 
     EkynaUi.prototype.init = function(aos) {
         // hack to fix jquery 3.6 focus security patch that bugs auto search in select-2
         // https://forums.select2.org/t/search-being-unfocused/1203/10
         $(document).on('select2:open', () => {
-            document.querySelector('.select2-search__field').focus();
+            document.querySelector('.select2-container--open .select2-search__field').focus();
         });
 
         AOS.init($.extend({}, aos, {
@@ -14,11 +14,11 @@ define(['require', 'jquery', 'aos', 'bootstrap', 'ekyna-clipboard-copy'], functi
         }));
 
         // Forms
-        var $forms = $('form');
+        const $forms = $('form');
         if (0 < $forms.length) {
             require(['ekyna-form'], function(Form) {
                 $forms.each(function(i, f) {
-                    var form = Form.create(f);
+                    const form = Form.create(f);
                     form.init();
                 });
             });
@@ -28,8 +28,7 @@ define(['require', 'jquery', 'aos', 'bootstrap', 'ekyna-clipboard-copy'], functi
         $(document).on('click', 'a[data-toggle-details]', function(e) {
             e.preventDefault();
 
-            var $this = $(this), $target = $('#' + $this.data('toggle-details'));
-
+            const $target = $('#' + $(this).data('toggle-details'));
             if (1 === $target.length) {
                 if ($target.is(':visible')) {
                     $target.hide();
